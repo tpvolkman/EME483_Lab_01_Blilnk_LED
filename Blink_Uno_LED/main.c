d91eb8ff02b48e8ce608f420bfca2858153aa087
Thomas Volkman <51494553+tpvolkman@users.noreply.github.com>
2022-09-10 19:16:43 -0500

    Add files via upload

---
+++ Blink_Uno_LED/main.c
@@ -0,0 +1,22 @@
+/*
+ *	Blink_Uno_LED.c
+ * 
+ *	Author: Professor Volkman
+ *  Description: This application blinks the LED on the UNO Development Board
+ */
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB |= 0x20;				// (1 << DDB5)
+
+	while (1)
+	{
+		PORTB ^= 0x20;			// (1<<PORTB5);
+		_delay_ms(1000);
+	}
+}
+
